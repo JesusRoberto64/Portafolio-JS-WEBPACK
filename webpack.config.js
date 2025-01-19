b1c46e68da5fda1481151af3ca9ac941aee9f950
Jesús Roberto Gzz Fonsca <gamerslive20@gmail.com>
2025-01-18 18:23:40 -0600

    new configurations done just clean the code

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,17 @@ const path = require('path');
 const CopyWebPackPlugin = require('copy-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+
 module.exports = {
     entry: './scr/js/main.js', // Punto de entrada 
     output: {
-        filename: 'bundle.js', //Archivo de salida
-        path: path.resolve(__dirname, 'dist/js'), //Directorio de salida
-        publicPath: '/Portafolio-JS-WEBPACK/', ///Portafolio-JS-WEBPACK/
+        filename: 'js/bundle.js', //Archivo de salida
+        path: path.resolve(__dirname, 'dist'), //Directorio de salida
+        publicPath: '/Portafolio-JS-WEBPACK/',
     },
     module: {
        rules: [
@@ -18,23 +23,35 @@ module.exports = {
                 loader: 'babel-loader' //usar loader de babel
             },
         },
+        {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        }
        ],
     },
     plugins: [
         new CopyWebPackPlugin({
             patterns: [
-                {from : 'scr/views', to: '../views'},
-                {from: 'scr/assets/css', to: '../assets/css'},
-                {from: 'scr/assets/images', to: '../assets/images'},
+                {from : 'scr/views', to: 'views'},
+                {from: 'scr/assets/css', to: 'assets/css'},
+                {from: 'scr/assets/images', to: 'assets/images'},
             ]
         }),
-        /*
         new HtmlWebPackPlugin({
             template: './scr/index.html',
-            publicPath: '../'
+            publicPath: './',
         }),
-        */
+        new MiniCssExtractPlugin({
+            filename: 'assets/css/[name].[contenthash].css', // Genera archivos CSS con un hash
+        }),
     ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin(),
+        ],
+    },
     devServer: {
         static:{
             directory: path.resolve(__dirname, 'dist'),
